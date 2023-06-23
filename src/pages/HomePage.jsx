bd324e6ef8b9b3b435b4de34b7d095600cd2f572
MaggieWesolowska <maggiewes07@gmail.com>
2023-06-23 20:23:56 +0200

    added home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import { useTrendingList } from '../utils/hooks/useTrendingList';
+import { TrendingList } from '../components/TrendingList';
+
+const HomePage = () => {
+	const { trendingMovies } = useTrendingList();
+	return (
+		<div>
+			<TrendingList movies={trendingMovies}></TrendingList>
+		</div>
+	);
+};
+
+export default HomePage;
