97a9f6427896b10999f61911191e7f2c3ee8dbdf
MaggieWesolowska <maggiewes07@gmail.com>
2023-06-23 19:47:55 +0200

    added api for trending

--- src/utils/api/apiMovieDB.jsx
+++ src/utils/api/apiMovieDB.jsx
@@ -1,1 +1,13 @@
 import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+const API_KEY = '?api_key=11517d6931d2b5137dd13eb45c72b984';
+
+export const getTrending = async () => {
+	const response = await axios.get(
+		`/trending/movie/day${API_KEY}`
+	);
+	return response.data.results.map(({ id, title }) => {
+		return { id, title };
+	});
+};
