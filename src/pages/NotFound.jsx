0dc6d676f4b5e876673afbcaa4dbb42b271d281d
MaggieWesolowska <maggiewes07@gmail.com>
2023-06-23 19:41:32 +0200

    added files and folders, installed axios

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+	return (
+		<main style={{ textAlign: 'center' }}>
+			<p style={{ fontSize: 72 }}>404</p>
+			<p>
+				Sorry, the movie you're looking for could not be
+				found, try again!
+			</p>
+		</main>
+	);
+};
+
+export default NotFound;
