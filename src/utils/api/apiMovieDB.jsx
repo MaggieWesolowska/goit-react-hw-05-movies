dbfc19de682ea6e016309457346b25d324c2b6ed
MaggieWesolowska <maggiewes07@gmail.com>
2023-06-23 20:07:27 +0200

    added api fetching

--- src/utils/api/apiMovieDB.jsx
+++ src/utils/api/apiMovieDB.jsx
@@ -11,3 +11,41 @@ export const getTrending = async () => {
 		return { id, title };
 	});
 };
+
+export const searchMovies = async inputValue => {
+	const response = await axios.get(
+		`/search/movie${API_KEY}&query=${inputValue}&language=en-US&page=1`
+	);
+	return response.data.results.map(({ id, title }) => {
+		return { id, title };
+	});
+};
+
+export const getMovieDetails = async movieId => {
+	const response = await axios.get(
+		`/movie/${movieId}${API_KEY}&language=en-US`
+	);
+	return response.data;
+};
+
+export const getMovieCredits = async moviesId => {
+	const response = await axios.get(
+		`/movie/${moviesId}/credits${API_KEY}&language=en-US`
+	);
+	return response.data.cast.map(
+		({ name, character, profile_path, id }) => {
+			return { name, character, profile_path, id };
+		}
+	);
+};
+
+export const getMovieReviews = async moviesId => {
+	const response = await axios.get(
+		`/movie/${moviesId}/reviews${API_KEY}&language=en-US&page=1`
+	);
+	return response.data.results.map(
+		({ author, content, id }) => {
+			return { author, content, id };
+		}
+	);
+};
