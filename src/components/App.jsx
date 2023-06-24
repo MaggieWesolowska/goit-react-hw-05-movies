cb70a977cebebd31bb173c7a1005a955a9d450e7
MaggieWesolowska <maggiewes07@gmail.com>
2023-06-24 15:01:56 +0200

    added components and pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,34 @@
 import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout.jsx';
 
 const HomePage = lazy(() => import('../pages/HomePage'));
+const MovieDetailsPage = lazy(() =>
+	import('../pages/MovieDetailsPage')
+);
+const Movies = lazy(() => import('../pages/Movies'));
+const NotFound = lazy(() => import('../pages/NotFound'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 const App = () => {
 	return (
-		<div style={{ padding: '40px' }}>
+		<div
+			style={{
+				padding: '40px',
+			}}>
 			<Routes>
-				<Route path="/" element={<HomePage />} />
+				<Route path="/" element={<SharedLayout />}>
+					<Route index element={<HomePage />} />
+					<Route path="movies" element={<Movies />} />
+					<Route
+						path="movies/:movieId"
+						element={<MovieDetailsPage />}>
+						<Route path="cast" element={<Cast />} />
+						<Route path="reviews" element={<Reviews />} />
+					</Route>
+				</Route>
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
 	);
