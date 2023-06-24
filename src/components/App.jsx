c63f08d1ba2e28fa9a2ef56d7a65b09618b986fa
MaggieWesolowska <maggiewes07@gmail.com>
2023-06-24 15:11:38 +0200

    edited app and added movie page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,16 @@ import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout.jsx';
 
-const HomePage = lazy(() => import('../pages/HomePage'));
+const HomePage = lazy(() =>
+	import('../pages/HomePage.jsx')
+);
 const MovieDetailsPage = lazy(() =>
 	import('../pages/MovieDetailsPage')
 );
-const Movies = lazy(() => import('../pages/Movies'));
+const MoviePage = lazy(() => import('../pages/MoviePage'));
 const NotFound = lazy(() => import('../pages/NotFound'));
-const Cast = lazy(() => import('./Cast/Cast'));
-const Reviews = lazy(() => import('./Reviews/Reviews'));
+const Cast = lazy(() => import('./Cast/Cast.jsx'));
+const Reviews = lazy(() => import('./Reviews/Reviews.jsx'));
 
 const App = () => {
 	return (
@@ -20,7 +22,7 @@ const App = () => {
 			<Routes>
 				<Route path="/" element={<SharedLayout />}>
 					<Route index element={<HomePage />} />
-					<Route path="movies" element={<Movies />} />
+					<Route path="movies" element={<MoviePage />} />
 					<Route
 						path="movies/:movieId"
 						element={<MovieDetailsPage />}>
---
+++ src/pages/MoviePage.jsx
@@ -0,0 +1,7 @@
+import { SearchMovie } from '../components/SearchMovie/SearchMovie';
+
+const MoviePage = () => {
+	return <SearchMovie />;
+};
+
+export default MoviePage;
